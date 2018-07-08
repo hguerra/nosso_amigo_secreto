a2fafdcfdae546ed10ab8c4154c08495ff3ad776
Heitor Guerra Carneiro <heitorgcarneiro@gmail.com>
2018-07-08 10:54:46 -0300

    Adding members_controller_spec#create

--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,26 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
+  before(:each) do
+    @request.env['devise.mapping'] = Devise.mappings[:user]
+    @current_user = FactoryBot.create(:user)
+    sign_in @current_user
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
+  describe 'POST #create' do
+    before(:each) do
+      request.env['HTTP_ACCEPT'] = 'application/json'
+
+      @campaign = create(:campaign, user: @current_user)
+      @member_attributos = attributes_for(:member, campaign_id: @campaign.id)
+    end
+
+    it 'Create new member' do
+      post :create, params: { member: @member_attributos }
+
       expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['id'].is_a?(Numeric)).to eq(true)
+    end
+
+    it 'Raise an error when creating a new member' do
+      @member_attributos[:campaign_id] = nil
+      post :create, params: { member: @member_attributos }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['campaign'][0]).to eq('must exist')
     end
   end
-
-  describe "GET #update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
