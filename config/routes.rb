3f9c06afcbc418c1352b7658fa1b7a498c16a3ac
Heitor Guerra Carneiro <heitorgcarneiro@gmail.com>
2018-07-10 19:42:27 -0300

    Redirect to campaigns when user authenticated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
+
+  authenticated :user do
+    root to: 'campaigns#index', as: :authenticated_root
+    # root to: redirect('/campaigns') # show the path
+  end
+
   # mount Sidekiq::Web => '/sidekiq'
 
   root to: 'pages#home'
